966e50ee5c8dcfdf5ffdda21bd62cf49dcd7e7a7
Mayank Shastri <mayankshastri2020@gmail.com>
2025-09-19 02:39:23 +0530

    Sum or product program

---
+++ Sum or Product.py
@@ -0,0 +1,12 @@
+def sum_or_product(a,b):
+    product=a*b
+    if product<=1000:
+        return product
+    else:
+        return a+b
+
+a=int(input("Enter your first number: "))
+b=int(input("Enter your second number: "))
+result=sum_or_product(a,b)
+
+print(f"The result is {result}")
